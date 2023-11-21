f696b9d300079e930612b2e5f1ae07b49fe93f48
andreev danya <lev.andreev98@mail.ru>
2023-11-21 20:58:06 +0300

    add all functions in .h and .cpp files

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,93 +1,9 @@
 #include <iostream>
-#include <cmath>
 #include <string>
-#include <vector>
 #include <fstream>
+#include "redactor.h"
 
 using namespace std;
-
-string StrInput() {
-	string temp;
-	getline(cin, temp);
-	return temp;
-}
-
-int CaseDialog() {
-	char temp;
-	cin >> temp;
-	switch (temp)
-	{
-	case 'y':
-		return 1;
-	case 'n':
-		return 0;
-	default:
-		return -1;
-	}
-}
-
-int DialogYN() {
-	setlocale(0, "rus");
-	int res = CaseDialog();
-	if ((res == 1) || (res == 0))
-		return res;
-	else {
-		cout << "Неверное значение! Повторите попытку!" << endl;
-		DialogYN();
-	}
-
-}
-
-
-string Corrector(string In, string badwords) {
-	setlocale(0, "rus");
-	string in = In;
-	if (in.find(badwords)!=string::npos) {
-		cout << "Было найдено запретное слово:" << badwords << endl;
-		cout << endl << endl << "Хотите заменить запретные слова, да или нет?";
-		int dial = DialogYN();
-		if (dial == 0) {
-			cout << "Удалить запретные слова?";
-			if (DialogYN() == 1)
-				while (in.find  (badwords) != string::npos) {
-					int pos = in.find(badwords);
-					int leng = badwords.size();
-					in.erase(pos, leng);
-				}
-			else
-			{
-			}
-		}
-		else
-		{
-
-			cout << "Введите слово на которое хотите заменить:";
-			string temp;
-			cin >> temp;
-			//string temp1 = StrInput();
-			while (in.find(badwords) != string::npos) {
-				int pos = in.find(badwords);
-				int leng = badwords.size();
-				in.erase(pos, leng);
-				in.insert(pos, temp);
-			}
-		}
-	}
-	else
-		cout << "Слово не найдено." << endl << endl;
-	return in;
-}
-
-string Bad(string str) {
-	setlocale(0, "rus");
-	cout << "Введите слово, которое хотите найти и скоректировать." << endl;
-	string bad= StrInput();
-
-	string temp =Corrector(str, bad);
-	cout << temp;
-	return temp;
-}
-
 
 int main() {
 	setlocale(0, "rus");
---
+++ lab4/redactor.h
@@ -0,0 +1,19 @@
+#ifndef REDACTOR_H
+#define REDACTOR_H
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string StrInput();
+int CaseDialog();
+int DialogYN();
+string Corrector(string In, string badwords);
+string Bad(string str);
+
+
+#endif
+
